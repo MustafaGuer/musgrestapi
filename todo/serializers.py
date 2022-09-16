0421ff9fa01b8ef18507fb40ca0febc64e57f98e
MustafaGuer <mustafaguer@yahoo.de>
2022-09-16 16:41:05 +0200

    add: UserSerializer

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
-from .models import Todo
+from .models import User, Todo
 
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta: 
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email']
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer()
+    # user = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'created_at'] # , 'user'
+        fields = ['id', 'title', 'description', 'created_at', 'user']
 
 
